e164fc1cfc39a49c92b61e0cb1b419706bacee23
superrowan002 <157720210+superrowan002@users.noreply.github.com>
2025-01-12 02:29:48 +0100

    Update mo's academy autowin lobby.lua

--- mo's academy autowin lobby.lua
+++ mo's academy autowin lobby.lua
@@ -1,1 +1,8 @@
+-- (Wait until loaded in
+-- Select student role)
+-- Teleport to most full bus
 
+queueteleport = (syn and syn.queue_on_teleport) or queue_on_teleport or (fluxus and fluxus.queue_on_teleport) -- Execute script on teleport
+game:GetService("Players").LocalPlayer.OnTeleport:Connect(function(State)
+    queueteleport("loadstring(game:HttpGet('https://raw.githubusercontent.com/superrowan002/scripts/refs/heads/main/mo's%20academy%20autowin.lua'))()")
+end)
