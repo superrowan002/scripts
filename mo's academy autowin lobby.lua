c62bb3fe539acc74f9150f4f680a1050eb640199
superrowan002 <157720210+superrowan002@users.noreply.github.com>
2025-01-13 18:19:42 +0100

    Update mo's academy autowin lobby.lua

--- mo's academy autowin lobby.lua
+++ mo's academy autowin lobby.lua
@@ -1,4 +1,21 @@
--- Teleport to most full bus
+local rootPart = game.Players.LocalPlayer.Character.HumanoidRootPart
+local function findFirstBus()
+    local busNames = {"Bus1", "Bus2", "Bus3"}
+    for _, busName in ipairs(busNames) do
+        local bus = workspace:FindFirstChild(busName)
+        if bus and bus:FindFirstChild("Teleporting") and bus.Teleporting:IsA("BoolValue") and bus.Teleporting.Value == false then
+            if busName == "Bus1" then
+                rootPart.CFrame = game.Workspace.JoinSensors.BusOneTouch.CFrame
+            elseif busName == "Bus2" then
+                rootPart.CFrame = game.Workspace.JoinSensors.BusTwoTouch.CFrame
+            elseif busName == "Bus3" then
+                rootPart.CFrame = game.Workspace.JoinSensors.BusThreeTouch.CFrame
+            end
+            return
+        end
+    end
+end
+findFirstBus()
 
 queueteleport = (syn and syn.queue_on_teleport) or queue_on_teleport or (fluxus and fluxus.queue_on_teleport) -- Execute script on teleport
 game:GetService("Players").LocalPlayer.OnTeleport:Connect(function(State)
